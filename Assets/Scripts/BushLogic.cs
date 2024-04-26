2a8348567d10db8fdc901b85b450620219b9cfea
Emre Erdoğan <emreedgan@gmail.com>
2024-04-26 23:44:00 +0300

    george bush stuff

--- Assets/Scripts/BushLogic.cs
+++ Assets/Scripts/BushLogic.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BushBounce : MonoBehaviour
+public class BushLogic : MonoBehaviour
 {
     // Start is called before the first frame update
     private CircleCollider2D collider;
+    private int collisionCounter;
+    private readonly int collisionLimit = 5;
     void Start()
     {
         collider = GetComponent<CircleCollider2D>();
         collider.sharedMaterial.bounciness = 0.7f;
         collider.sharedMaterial.friction = 0.4f;
+        collisionCounter = 0;
     }
 
     // Update is called once per frame
@@ -20,11 +23,21 @@ public class BushBounce : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if(collisionCounter == 5)
+        {
+            //Oncesinde belki yok olma animasyonu yaparýz
+            Destroy(gameObject);
+        }
+        if(collision.gameObject.tag == "Player")
+        {
+            //olum ya da health azalt
+        }
         if(collision.gameObject.tag == "Ground")
         {
+            collisionCounter++;
             collider.sharedMaterial.bounciness = Random.Range(0.7f,1.2f);
             collider.sharedMaterial.friction = Random.Range(0.4f, 0.7f);
-
         }
+
     }
 }
