548858a6cbb3506a23ecce37ab48e40c24688231
Emre Erdoğan <emreedgan@gmail.com>
2024-04-27 01:32:15 +0300

    a

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -11,7 +11,7 @@ public class PlayerHealth : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        health = 1;
+        health = 3;
         invincible = false;
     }
 
