c9700ed92f38a09f0f879e1f5e9f4453a92759dd
Adam Sattout <adamsattout@Yahoo.com>
2024-04-27 02:07:52 +0300

    Interactables Added

---
+++ Assets/Scripts/InteractScript.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InteractScript : MonoBehaviour
+{
+    public bool inRange;
+    public UnityAction action;
+    public KeyCode key;
+    public TextMesh text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inRange)
+        {
+            if (Input.GetKeyDown(key))
+            {
+                action.Invoke();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.name == "Player")
+        {
+            inRange = true;
+            Debug.Log("In");
+            text.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.name == "Player")
+        {
+            inRange = false;
+            Debug.Log("Out");
+            text.gameObject.SetActive(false);
+        }
+    }
+}
