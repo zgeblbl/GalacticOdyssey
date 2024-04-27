20301251ca280a2393dfc3c73b320ac3f00b36ae
AbdullahEdik <edikabdullah2@gmail.com>
2024-04-28 01:13:54 +0300

    MainMenuButtons

---
+++ Assets/Scripts/SceneMechanics/ButtonManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
